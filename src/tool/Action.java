19d4073c297b51c2a6dac2a3d9a94147cd28b139
h-onishi44 <fko2447080@stu.o-hara.ac.jp>
2025-04-16 14:14:21 +0900

    tool

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -1,5 +1,10 @@
 package tool;
 
-public class Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public abstract class Action {
+    public abstract void execute(
+        HttpServletRequest req, HttpServletResponse res
+    ) throws Exception;
 }
--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -1,11 +1,55 @@
 package tool;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(urlPatterns={"#"})
-//public class FrontController extends HttpServlet{
-//
-//	public void doPost(HttpServletRequest,HttpServletResponse response
-//		    ) throws ServletException, IOException)
-//}
-//}
+/**
+ * フロントコントローラ（*.actionにマッチ）
+ */
+@WebServlet(urlPatterns = {"*.action"})
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        System.out.println("FrontController!");
+
+        try {
+            // リクエストされたパスを取得
+            String path = request.getServletPath().substring(1);  // 例: "chapter23/Search.action"
+
+            // アクションクラスの名前に変換
+            String name = path.replace(".a", "A").replace('/', '.');  // 例: "chapter23.SearchAction"
+
+            // アクションクラスを動的にロードしてインスタンス生成
+            Action action = (Action) Class.forName(name)
+                .getDeclaredConstructor()
+                .newInstance();
+
+            // アクションを実行（戻り値はなし）
+            action.execute(request, response);
+
+            // 各アクションクラス側で forward などを行う前提
+
+        } catch (Exception e) {
+            // エラー出力
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<h2>エラー発生</h2>");
+            e.printStackTrace(out);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
