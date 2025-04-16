a664691ab91edb10bdafc757081b3f6c59f06175
t-sakaguchi <fko2447070@stu.o-hara.ac.jp>
2025-04-16 09:32:16 +0900

    toolファイルのコメントアウト

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -2,10 +2,10 @@ package tool;
 
 
 
-@WebServlet(urlPatterns={"#"})
-public class FrontController extends HttpServlet{
-
-	public void doPost(HttpServletRequest,HttpServletResponse response
-		    ) throws ServletException, IOException)
-}
-}
+//@WebServlet(urlPatterns={"#"})
+//public class FrontController extends HttpServlet{
+//
+//	public void doPost(HttpServletRequest,HttpServletResponse response
+//		    ) throws ServletException, IOException)
+//}
+//}
