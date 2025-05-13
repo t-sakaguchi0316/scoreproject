d0a023b253dc4d61130a6ceb0b4ca8c92f0ee0c9
h-onishi44 <fko2447080@stu.o-hara.ac.jp>
2025-05-13 10:36:49 +0900

    TestListSubjectの修正

--- src/bean/TestListSubject.java
+++ src/bean/TestListSubject.java
@@ -2,46 +2,63 @@ package bean;
 
 import java.io.Serializable;
 import java.util.Map;
-//ゲッターセッターが足りない
-public class TestListSubject implements Serializable{
-	private int entYear;
-	private String studentNo;
-	private String studentName;
-	private String classNum;
-	private Map<Integer, Integer> points;
 
-	public int getEntYear(){
-		return entYear;
-	}
-	public String getStundentNo(){
-		return studentNo;
-	}
-	public String getStudentName(){
-		return studentName;
-	}
-	public String  getClassNum(){
-		return classNum;
-	}
-	public Map<Integer,Integer> getPoints(){
-		return points;
-	}
+/**
+ * 学生ごとの科目別得点を保持するBean
+ */
+public class TestListSubject implements Serializable {
+    private int entYear;
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+    private Map<Integer, Integer> points;
 
+    public int getEntYear() {
+        return entYear;
+    }
 
-	public void setEntYear(int entYear){
-		this.entYear=entYear;
-	}
-	public void setStudentNo(String studentNo){
-		this.studentNo=studentNo;
-	}
-	public void setStudentName(String studentName){
-		this.studentName=studentName;
-	}
-	public void setClassNum(String classNum){
-		this.classNum=classNum;
-	}
-	public void setPoinst(Map<Integer, Integer> points){
-		this.points=points;
-	}
+    public void setEntYear(int entYear) {
+        this.entYear = entYear;
+    }
 
+    public String getStudentNo() {
+        return studentNo;
+    }
 
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public Map<Integer, Integer> getPoints() {
+        return points;
+    }
+
+    public void setPoints(Map<Integer, Integer> points) {
+        this.points = points;
+    }
+
+    /**
+     * 科目コード(subjectCd)と得点(point)をマップに追加する
+     */
+    public void putPoint(Integer subjectCd, Integer point) {
+        if (this.points != null) {
+            this.points.put(subjectCd, point);
+        }
+    }
 }
